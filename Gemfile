7cf848adb485897a247350c5f5a742af0eace19f
Jeff Douglas <jeff@jeffdouglas.com>
2012-01-27 07:19:16 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'sqlite3'
+gem 'databasedotcom'
+gem 'databasedotcom-rails'
+gem 'heroku'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SfdcRailsDemo::Application.config.secret_token = '45b4258f4edb5ce6b6a887b78bed34992774065eea8d6c92f0799ae6e0949e5a4c24c427a9c1d8682678b3403268ecae716cbf910bb37741ab79fb6cc56823d5'
